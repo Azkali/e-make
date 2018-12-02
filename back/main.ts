6777a22cf54d820225c1b78954723c4f3397ebb5
Gerkin <nihilivin@gmail.com>
2018-12-02 12:37:19 +0100

    feature: Updated Diaspora version, declared new models

--- back/main.ts
+++ back/main.ts
@@ -2,18 +2,35 @@ import { Diaspora } from '@diaspora/diaspora';
 import { ExpressApiGenerator } from '@diaspora/plugin-server';
 import express = require( 'express' );
 
-import {product} from '../cross/models/product';
+import {product, user, attributeCategory, attribute} from '../cross/models';
 
 Diaspora.createNamedDataSource( 'main', 'inMemory' );
-Diaspora.declareModel( 'Product', {
+const Product = Diaspora.declareModel( 'Product', {
 	sources: 'main',
 	attributes: product,
+} );
+const User = Diaspora.declareModel( 'User', {
+	sources: 'main',
+	attributes: user,
+} );
+const AttributeCategory = Diaspora.declareModel( 'AttributeCategory', {
+	sources: 'main',
+	attributes: attributeCategory,
+} );
+const Attribute = Diaspora.declareModel( 'Attribute', {
+	sources: 'main',
+	attributes: attribute,
 } );
 
 const apiMiddleware = new ExpressApiGenerator( {
 	webserverType: 'express',
 	models: {
 		Product: true,
+		User: true,
+		AttributeCategory: {
+			plural: 'attributecategories',
+		},
+		Attribute: true,
 	},
 } );
 
@@ -21,8 +38,7 @@ const apiMiddleware = new ExpressApiGenerator( {
 const app = express();
 app.use( '/api', apiMiddleware.middleware );
 
-const port = 8765;
-const host = '0.0.0.0';
-const httpServer = app.listen( port, host, () => {
-	console.log( `Example app listening on port ${port}!` );
+const backConfig = require( '../cross/config/local/back.json' );
+const httpServer = app.listen( backConfig.port, backConfig.host, () => {
+	console.log( `Example app listening on ${backConfig.host}:${backConfig.port}!` );
 } );
