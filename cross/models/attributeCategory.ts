f098202f4c05d5d8c9d10944ee11a56e60083f59
Gerkin <nihilivin@gmail.com>
2018-12-06 20:44:01 +0100

    chore(cross): Created missing cross config files, added model interfaces

--- cross/models/attributeCategory.ts
+++ cross/models/attributeCategory.ts
@@ -1,7 +1,9 @@
 import { EFieldType } from "@diaspora/diaspora";
 import { Raw } from "@diaspora/diaspora/dist/types/types/modelDescription";
 
-
+export interface IAttributeCategory{
+    name?: string;
+}
 export const attributeCategory: Raw.IAttributesDescription = {
 	name: EFieldType.STRING,
 };
--- cross/models/user.ts
+++ cross/models/user.ts
@@ -2,9 +2,33 @@ import { EFieldType } from "@diaspora/diaspora";
 import { Raw } from "@diaspora/diaspora/dist/types/types/modelDescription";
 
 
+export enum EAuthorization{
+	User = 0b0,
+	Admin = 0b11111,
+}
+export interface IUser{
+	email?: string;
+	password?: string;
+	authorizations: EAuthorization;
+	
+	facebookId?: string;
+	facebookToken?: string;
+	
+	googleId?: string;
+	googleToken?: string;
+}
 export const user: Raw.IAttributesDescription = {
-    email: EFieldType.STRING,
-    password: EFieldType.STRING,
-    facebookId: EFieldType.STRING,
-    facebooktoken: EFieldType.STRING,
+	email: EFieldType.STRING,
+	password: EFieldType.STRING,
+	authorizations: {
+		type: EFieldType.INTEGER,
+		required: true,
+		default: EAuthorization.User,
+	},
+	
+	facebookId: EFieldType.STRING,
+	facebookToken: EFieldType.STRING,
+	
+	googleId: EFieldType.STRING,
+	googleToken: EFieldType.STRING,
 };
