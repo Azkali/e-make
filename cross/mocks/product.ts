2a64793e408e90caa2d4db09eba035ccd3b266c2
Gerkin <nihilivin@gmail.com>
2018-12-16 01:46:58 +0100

    feat(mocks): Added products mocks

---
+++ cross/mocks/product.ts
@@ -0,0 +1,12 @@
+import { IProduct } from './../models/product';
+
+export const products: IProduct[] = [
+    {
+        name: "Mango",
+        images: ["https://via.placeholder.com/576x515.png?text=Mango"],
+    },
+    {
+        name: "Banana",
+        images: ["https://via.placeholder.com/576x515.png?text=Banana"],
+    }
+]
