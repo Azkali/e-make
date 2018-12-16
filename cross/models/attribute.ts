993d55d832103cce5aa84fbf88c36fa0063dd80c
Gerkin <nihilivin@gmail.com>
2018-12-16 16:27:23 +0100

    feat(mocks): Added relations, added mocks for attributes, added loadMock

--- cross/models/attribute.ts
+++ cross/models/attribute.ts
@@ -1,11 +1,21 @@
-import { EFieldType } from "@diaspora/diaspora";
+import { IAttributeCategory } from './attributeCategory';
+import { EFieldType, EntityUid } from "@diaspora/diaspora";
 import { Raw } from "@diaspora/diaspora/dist/types/types/modelDescription";
 
 export interface IAttribute{
-    name?: string;
-    price?: number;
+	name?: string;
+	price: number;
+	categoryId: EntityUid;
+	category: IAttributeCategory
 }
 export const attribute: Raw.IAttributesDescription = {
-    name: EFieldType.STRING,
-    price: EFieldType.FLOAT,
+	name: EFieldType.STRING,
+	price: {
+		type: EFieldType.FLOAT,
+		required: true
+	},
+	categoryId: {
+		type: EFieldType.STRING,
+		required: true,
+	},
 };
--- cross/models/product.ts
+++ cross/models/product.ts
@@ -1,20 +1,47 @@
-import { attributeCategory } from './attributeCategory';
-import { EFieldType } from "@diaspora/diaspora";
+import { IAttributeCategory } from './attributeCategory';
+import { EFieldType, EntityUid } from "@diaspora/diaspora";
 import { Raw } from "@diaspora/diaspora/dist/types/types/modelDescription";
 
 export interface IProduct{
-    name?: string;
-    attributeCategory?: number[];
-    images?: string[];
+	name?: string;
+	customizableParts?: {
+		factor: number;
+		name: string;
+		categoryId: EntityUid;
+		category?: IAttributeCategory;
+	}[];
+	images?: string[];
+	basePrice: number;
 }
 export const product: Raw.IAttributesDescription = {
 	name: EFieldType.STRING,
-	attributeCategories: {
+	customizableParts: {
 		type: EFieldType.ARRAY,
-		of: EFieldType.INTEGER,
+		of: {
+			type: EFieldType.OBJECT,
+			attributes: {
+				factor: {
+					type: EFieldType.INTEGER,
+					required: true,
+					default: 1,
+				},
+				name: {
+					type: EFieldType.STRING,
+					required: true,
+				},
+				categoryId: {
+					type: EFieldType.STRING,
+					required: true,
+				}
+			}
+		},
 	},
 	images: {
 		type: EFieldType.ARRAY,
 		of: EFieldType.STRING,
 	},
+	basePrice: {
+		type: EFieldType.FLOAT,
+		required: true,
+	}
 };
