db7f127ae502a92a7f79c17852b72ecf6c2cd7a5
Gerkin <nihilivin@gmail.com>
2019-03-10 19:31:24 +0100

    fix(Back/mailer): Fixed style, template & config passed to templates

--- back/src/mailer.ts
+++ back/src/mailer.ts
@@ -1,29 +1,29 @@
+import { castArray, compact, Dictionary, map, mapValues, toPairs, values } from 'lodash';
 import nodemailer, { SendMailOptions } from 'nodemailer';
-import { toPairs, map, castArray, mapValues, values, compact, Dictionary } from 'lodash';
-import nunjucks from 'nunjucks';
-import { join, resolve } from 'path';
-import numeral from 'numeral';
 import { assign } from 'nodemailer/lib/shared';
+import numeral from 'numeral';
+import nunjucks from 'nunjucks';
+import { join, resolve as pathResolve } from 'path';
 
-import { logger } from './logger';
 import { inspect } from 'util';
+import { logger } from './logger';
 
-import { IQuote } from '../cross/models';
+import { IBackConfig } from '../cross/config/config-types';
 import { backConfig } from '../cross/config/environments/loader';
 import { makeAbsoluteUrl } from '../cross/config/utils';
-import { IBackConfig } from '../cross/config/config-types';
+import { IQuote } from '../cross/models';
 
 const env = new nunjucks.Environment(
 	new nunjucks.FileSystemLoader(
-		resolve( __dirname, '../templates' ),
-		{ noCache: !backConfig.common.production }
+		pathResolve( __dirname, '../templates' ),
+		{ noCache: !backConfig.common.production },
 	),
-	{ autoescape: true, trimBlocks: true, noCache: !backConfig.common.production }
+	{ autoescape: true, trimBlocks: true, noCache: !backConfig.common.production },
 );
 
-env.addFilter( 'fitPad', ( input: unknown, size: number, {pad = ' ', keepWord = true, ellipsis = '...'} = {} ) => {
+env.addFilter( 'fitPad', ( input: unknown, size: number, { pad = ' ', keepWord = true, ellipsis = '...' } = {} ) => {
 	const str = `${input}`;
-	if ( str.length + ellipsis.length > size ){
+	if ( str.length + ellipsis.length > size ) {
 		return ( env.getFilter( 'truncate' ) as any )( str, size, keepWord, ellipsis );
 	} else {
 		return str + pad.repeat( size - str.length );
@@ -31,20 +31,21 @@ env.addFilter( 'fitPad', ( input: unknown, size: number, {pad = ' ', keepWord = 
 } );
 const defaultFormat = '$0,0.00';
 env.addFilter( 'currency', ( value: number, format?: string ) => {
-	if ( !format ){
+	if ( !format ) {
 		format = defaultFormat;
 	}
 	return numeral( value ).format( format );
 } );
 
-
 const generateTransport = async ( mailerConfig: IBackConfig.IMailConfig.IMailAccountConfig ) => nodemailer.createTransport( {
 	host: mailerConfig.host,
 	port: mailerConfig.port || 587,
-	secure: false, // true for 465, false for other ports
+	 // true for 465, false for other ports
+	secure: false,
+
 	auth: {
-		user: mailerConfig.user,
 		pass: mailerConfig.password,
+		user: mailerConfig.user,
 	},
 	tls: {
 		rejectUnauthorized: false,
@@ -56,15 +57,17 @@ const generateTestTransport = async () =>
 			if ( err ) {
 				return reject( err );
 			}
-			
+
 			// create reusable transporter object using the default SMTP transport
 			return resolve( nodemailer.createTransport( {
 				host: 'smtp.ethereal.email',
 				port: 587,
-				secure: false, // true for 465, false for other ports
+				// true for 465, false for other ports
+				secure: false,
+
 				auth: {
-					user: account.user, // generated ethereal user
 					pass: account.pass,  // generated ethereal password
+					user: account.user, // generated ethereal user
 				},
 			} ) );
 		 } ) );
@@ -73,32 +76,34 @@ const getEnvTransport = async () => backConfig.mail.smtpAuth ?
 	generateTransport( backConfig.mail.smtpAuth ) :
 	generateTestTransport();
 
-
 const generateSenderLine = ( name: string, email: string ) =>
 	`"${name}" <${email}>`;
 
 const sendMail = async ( transport: nodemailer.Transporter, to: IBackConfig.IMailConfig.IMailAddress[], templateName: string, templateArgs: Dictionary<any> ) => {
-	const preparedTemplateArgs = assign( {config: backConfig.common.back, makeAbsoluteUrl}, templateArgs ) as Dictionary<any>;
+	const preparedTemplateArgs = assign( { config: backConfig.common, makeAbsoluteUrl }, templateArgs ) as Dictionary<any>;
 	preparedTemplateArgs.title = env.render( join( templateName, 'subject.txt' ), preparedTemplateArgs ).trim();
-	console.log( inspect( preparedTemplateArgs, {colors: true, depth: 15} ) );
+	// tslint:disable-next-line: no-console
+	console.log( inspect( preparedTemplateArgs, { colors: true, depth: 15 } ) );
 
 	// Render all templates
 	const mailOptions = {
-		to: values( mapValues( to, ( { name, email } ) => generateSenderLine( name, email ) ) ),
 		from: generateSenderLine( backConfig.mail.mailBot.name, backConfig.mail.mailBot.email ),
+		to: values( mapValues( to, ( { name, email } ) => generateSenderLine( name, email ) ) ),
+
 		subject: preparedTemplateArgs.title,
-		text: env.render( join( templateName, 'mail.txt' ), preparedTemplateArgs ).trim(),
+
 		html: env.render( join( templateName, 'mail.html' ), preparedTemplateArgs ).trim(),
+		text: env.render( join( templateName, 'mail.txt' ), preparedTemplateArgs ).trim(),
 	};
-	try{
-		const resInfos = await new Promise<any>( ( resolve,reject ) =>
+	try {
+		const resInfos = await new Promise<any>( ( resolve, reject ) =>
 			transport.sendMail( mailOptions, ( err, info ) => err ? reject( err ) : resolve( info ) ) );
 		const recipients = castArray( mailOptions.to ).join( ', ' );
 		const previewUrl = nodemailer.getTestMessageUrl( resInfos );
 		logger.debug( `Succesfully sent mail "${mailOptions.subject}" to ${recipients}.` +
 			( backConfig.mail.smtpAuth ? '' : ` Preview url: ${previewUrl}` ) );
 		return resInfos;
-	} catch ( e ){
+	} catch ( e ) {
 		logger.error( 'An error occured while sending a mail:', e.message );
 		throw e;
 	}
@@ -114,8 +119,8 @@ export const sendQuoteMails = async ( quote: IQuote, copyToUser: boolean ) => {
 };
 export const sendQuoteMail = async ( templateName: 'quote-admin' | 'quote-user', quote: IQuote, transport?: nodemailer.Transporter ) => {
 	const defaultedTransport = transport ? transport : await getEnvTransport();
-	const infos = await sendMail( defaultedTransport, backConfig.mail.quoteRecipients, templateName, {quote} );
-	if ( !transport ){
+	const infos = await sendMail( defaultedTransport, backConfig.mail.quoteRecipients, templateName, { quote } );
+	if ( !transport ) {
 		defaultedTransport.close();
 	}
 	return infos;
